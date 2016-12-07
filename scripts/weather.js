4981cd0d930b051c071f75a3ba0a414b315a5e3e
Hazumi <shirohazumi@openmailbox.org>
2016-12-07 17:27:25 -0500

    added location, weather, and humidity

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-    var city;
+  var city;
 	var country;
 	var $main = $('#main');
 
-    $.ajax({
-		url: "http://ip-api.com/json",
-		dataType: 'json',
-		method: 'GET',
+  $.ajax({
+  	url: "http://ip-api.com/json",
+  	dataType: 'json',
+  	method: 'GET',
 	}).done(function(data){
 		city = data.city;
 		country = data.countryCode;
 	});
-var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+country+"&appid=072a42af4eb157714427a895c8f71581&units=imperial"
+
+  var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+country+"&appid=072a42af4eb157714427a895c8f71581&units=imperial";
 
 	$.ajax({
 		url: url,
@@ -20,9 +21,13 @@ var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+country+"&app
 	}).done(function(data) {
 		console.log(data);
 		console.log(data.main.temp)
+    $main.append("Location " + data.name + "<br>");
+    $main.append("Weather " + data.weather[0].main + "<br>")
 		$main.append("Temp " + data.main.temp + "<br>");
-		$main.append("Wind " +data.wind.speed + "<br>");
+    $main.append("Humidity " + data.main.humidity + "<br>");
+		$main.append("Wind " + data.wind.speed + "<br>");
 		$main.append("Clouds " +data.clouds.all);
+    
 	});
 
 });
