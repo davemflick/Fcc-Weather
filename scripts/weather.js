c5bb9e6573fdaac4c2cd46f23057237fb8d17b46
Hazumi <shirohazumi@openmailbox.org>
2016-12-07 23:23:33 -0500

    displays forcast data for next 5 days

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,23 +7,24 @@ $(document).ready(function() {
   	url: "http://ip-api.com/json",
   	dataType: 'json',
   	method: 'GET',
-	}).done(function(locInfo){
-	 var city = locInfo.city;
-	 var data = locInfo.countryCode;
-   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + country + "&appid=072a42af4eb157714427a895c8f71581&units=imperial";
-   var forcastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + country + "&appid=072a42af4eb157714427a895c8f71581&units=imperial";
+	}).done(function(loc) {
+    var city = loc.city;
+    var data = loc.countryCode;
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + country + "&appid=072a42af4eb157714427a895c8f71581&units=imperial";
+    var forcastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + country + "&appid=072a42af4eb157714427a895c8f71581&units=imperial";
 
    $.ajax({
      url: url,
      dataType: 'jsonp',
      method: 'GET',
    }).done(function(data) {
-     $main.append("Location " + data.name + "<br>");
-     $main.append("Weather " + data.weather[0].main + "<br>")
-     $main.append("Temp " + data.main.temp + "<br>");
-     $main.append("Humidity " + data.main.humidity + "<br>");
-     $main.append("Wind " + data.wind.speed + "<br>");
-     $main.append("Clouds " + data.clouds.all);
+     $main.append("<h2>" + data.name + "</h2>");
+     $main.append("<h3>Today</h3>");
+     $main.append("Weather: " + data.weather[0].main + "<br>")
+     $main.append("Temp: " + data.main.temp + "<br>");
+     $main.append("Humidity: " + data.main.humidity + "<br>");
+     $main.append("Wind: " + data.wind.speed + "<br>");
+     $main.append("Clouds: " + data.clouds.all + "<br>");
    });
 
    $.ajax({
@@ -31,20 +32,17 @@ $(document).ready(function() {
       dataType: 'jsonp',
       method: 'GET',
     }).done(function(data) {
-      console.log(data);
+      var one = $('#1');
+      var two = $('#2');
+      for (var i = 0; i < data.list.length; i += 8) {
+        // console.log(data.list[i]);
+        $main.append("<h3>" + data.list[i].dt_txt.substring(5, 10) + "</h3>");
+        $main.append("Weather: " + data.list[i].weather[0].main + "<br>");
+        $main.append("Temp: " + data.list[i].main.temp + "<br>");
+        $main.append("Humidity: " + data.list[i].main.humidity + "<br>");
+        $main.append("Wind: " + data.list[i].wind.speed + "<br>");
+        $main.append("Clouds: " + data.list[i].clouds.all + "<br>");
+      }
     });
-
-
 	});
-
-
-
-
-
-
-
-
-
-
-
 });
